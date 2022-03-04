b31bd95b5a8a618ab33b54ce61c329f904b53f07
Gnexcoin <97846937+gnexcoin@users.noreply.github.com>
2022-03-04 13:17:36 +0600

    Update test.py

--- docker/history/test.py
+++ docker/history/test.py
@@ -6,10 +6,10 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 fullnodes = [
     #'http://10.60.103.43:8080',
-    'https://api.steemit.com',
+    'http://gnexportal.com:9000',
 ]
 rpc = Steem(fullnodes)
-mongo = MongoClient("mongodb://47.240.97.170")
+mongo = MongoClient("mongodb://195.181.245.47")
 #mongo = MongoClient("mongodb://10.40.103.102")
 db = mongo.steemdb
 
